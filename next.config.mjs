b31d6e15d060461587515139f12f9637292829b3
SuosPhearith <suosphearith@gmail.com>
2024-07-08 08:23:09 +0700

    for deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,30 +1,31 @@
 /** @type {import("next").NextConfig} */
 const nextConfig = {
+  distDir: "build",
   images: {
     domains: ["localhost"],
     remotePatterns: [
       {
         protocol: "https",
         hostname: "cdn.sanity.io",
-        port: ""
+        port: "",
       },
       {
         protocol: "https",
         hostname: "lh3.googleusercontent.com",
-        port: ""
+        port: "",
       },
       {
         protocol: "https",
         hostname: "avatars.githubusercontent.com",
-        port: ""
+        port: "",
       },
       {
         protocol: "https",
         hostname: "pub-b7fd9c30cdbf439183b75041f5f71b92.r2.dev",
-        port: ""
-      }
-    ]
-  }
+        port: "",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
